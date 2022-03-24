191aa3e485f038aa1c9fd289da03ef269b206ad6
Aleksandr Butakov <anbngm@gmail.com>
2022-03-24 19:38:03 +0500

    Added two methods

--- src/main/java/ru/anbn/pft/MyFirstProgram.java
+++ src/main/java/ru/anbn/pft/MyFirstProgram.java
@@ -4,6 +4,23 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int i = 4;
+        hello("Hello world!");
+        System.out.println(area(5));
+        System.out.println(area(5, 8));
+
+    }
+
+    private static void hello(String st) {
+        System.out.println(st);
+    }
+
+    private static double area(double len) {
+        return len*len;
+    }
+
+    private static double area(double len, double l) {
+        return len*l;
     }
 
 }
