53ce639d4296b0e0874822c2713dd4e6732a791f
Aleksandr Butakov <anbngm@gmail.com>
2022-03-25 11:04:05 +0500

    Added methods

--- src/main/java/ru/anbn/pft/MyFirstProgram.java
+++ src/main/java/ru/anbn/pft/MyFirstProgram.java
@@ -1,8 +1,81 @@
 package ru.anbn.pft;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+
+        Point pointA = new Point();
+        Point pointB = new Point();
+
+        // введем в консоли координаты точки A
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите координаты точки А на двумерной плоскости разрешением 800 х 600");
+
+        try {
+            System.out.print("x (0 ... 800) = ");
+            pointA.x = in.nextInt();
+            System.out.print("y (0 ... 600) = ");
+            pointA.y = in.nextInt();
+        } catch (InputMismatchException e) {
+            completionProgram();
+        }
+
+        // проверка корректности координат
+        checkingCorrectnessCoordinates(pointA.x, pointA.y);
+
+        // введем в консоли координаты точки B
+        System.out.println("Введите координаты точки B на двумерной плоскости разрешением 800 х 600");
+
+        try {
+            System.out.print("x (0 ... 800) = ");
+            pointB.x = in.nextInt();
+            System.out.print("y (0 ... 600) = ");
+            pointB.y = in.nextInt();
+        } catch (InputMismatchException e) {
+            completionProgram();
+        }
+
+        // проверка корректности координат
+        checkingCorrectnessCoordinates(pointB.x, pointB.y);
+
+        // вычислим расстояние между точками
+        distance(pointA, pointB);
+
+
+    }
+
+    // проверим что введенные координаты лежат в диапазоне 800 х 600
+    static void checkingCorrectnessCoordinates(int x, int y) {
+        if (x >= 0 & x <= 800 & y >= 0 & y <= 600) {
+            // координаты лежат в требуемом диапазоне
+        } else {
+            // введены некорректные данные
+            completionProgram();
+        }
+    }
+
+    // вычислим расстояние между точками
+    public static void distance(Point A, Point B) {
+        System.out.println(A.x);
+        System.out.println(A.y);
+        System.out.println(B.x);
+        System.out.println(B.y);
+
+
+    }
+
+
+    // введены некорректные данные, завершение выполнения программы
+    static void completionProgram() {
+        System.out.println("Введены некорректные данные, запустите программу заново :-(");
+        System.exit(0);
+    }
+
+
+        /*
         System.out.println("Hello world!");
         int i = 4;
         hello("Hello world!");
@@ -28,6 +101,7 @@ public class MyFirstProgram {
     private static double area(Rectangle r) {
         return r.a + r.b;
     }
+ */
 
-    
+
 }
---
+++ src/main/java/ru/anbn/pft/Point.java
@@ -0,0 +1,12 @@
+package ru.anbn.pft;
+
+// класс для представления точек на двумерной плоскости 800 х 600
+public class Point {
+
+    // координаты точки
+    public int x;
+    public int y;
+
+
+
+}
